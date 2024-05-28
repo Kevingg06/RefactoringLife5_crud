bb36c1fa4aa830542d02c2b4216908694edf3600
KarenAndrea24 <134449861+KarenAndrea24@users.noreply.github.com>
2024-05-28 09:14:14 -0500

    cambios

--- app/src/main/java/com/example/myapplication/utils/Inventory.kt
+++ app/src/main/java/com/example/myapplication/utils/Inventory.kt
@@ -3,5 +3,5 @@ package com.example.myapplication.utils
 import com.example.myapplication.model.Product
 
 object Inventory {
-    val products = mutableListOf<Product>(Product("123456", "Pepe", "https://http2.mlstatic.com/D_NQ_NP_620148-MLV74432754074_022024-O.webp", 1))
+    val products = mutableListOf<Product>()
 }
