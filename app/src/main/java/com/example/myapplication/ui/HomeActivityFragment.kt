c95537a2c5fbfc51d355ad5e295ca7b754d47628
KarenAndrea24 <134449861+KarenAndrea24@users.noreply.github.com>
2024-05-29 17:59:51 -0500

    Update

--- app/src/main/java/com/example/myapplication/ui/HomeActivityFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/HomeActivityFragment.kt
@@ -28,11 +28,6 @@ class HomeActivityFragment : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarHomeActivityFragment.toolbar)
 
-        binding.appBarHomeActivityFragment.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_home_activity_fragment)
--- app/src/main/java/com/example/myapplication/ui/ProductViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/ProductViewModel.kt
@@ -8,39 +8,6 @@ import androidx.lifecycle.ViewModel
 import com.example.myapplication.repository.RepositoryImp
 import com.example.myapplication.model.Product
 
-//class ProductViewModel(private val repositoryImp: RepositoryImp) : ViewModel() {
-//
-//    fun getProducts(): List<Product> {
-//        return repositoryImp.getProducts()
-//    }
-//
-//    fun addProduct(product: Product) {
-//        repositoryImp.addProduct(product)
-//    }
-//
-//    fun updateProduct(product: Product) {
-//        repositoryImp.updateProduct(product)
-//    }
-//
-//    fun deleteProduct(productId: String) {
-//        repositoryImp.deleteProduct(productId)
-//    }
-//
-//    fun searchProducts(query: String?): List<Product> {
-//        return if (query.isNullOrEmpty()) {
-//            getProducts()
-//        } else {
-//            getProducts().filter {
-//                it.name.contains(query, true) || it.id.contains(query, true)
-//            }
-//        }
-//    }
-//
-//}
-
-
-
-
 class ProductViewModel(application: Application) : AndroidViewModel(application) {
 
     private val sharedPreferences = application.getSharedPreferences("products", Application.MODE_PRIVATE)
